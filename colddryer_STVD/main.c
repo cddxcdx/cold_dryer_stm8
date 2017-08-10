0e3dd0a75347c5076442b682076d4f35f3f1c138
cddxcdx <cddxcdx@126.com>
2017-08-10 11:22:44 +0800

    fixed the high temperature display bug.

--- colddryer_STVD/main.c
+++ colddryer_STVD/main.c
@@ -11,6 +11,7 @@
 main(){
 	int16_t i;
 	int16_t current_tem_offset = 0;
+	uint16_t NTC_Voltage_Value = 0;
 	system_init();
 	EE_Parameters_Read();
 	while (1){
@@ -20,8 +21,9 @@ main(){
 		if(Tem_Update_Flag){		
 			Tem_Update_Flag = FALSE;
 			for(i = 0; i < 1451; i++){
-				if(NTC_Conversion_Value > Resolution)NTC_Conversion_Value = Resolution;
-				if(TEMP_TABLE[i] > (uint16_t)((float)NTC_Conversion_Value/Resolution*VREF)){
+				NTC_Voltage_Value = (uint16_t)((float)NTC_Conversion_Value/Resolution*VREF);
+				if(NTC_Voltage_Value > 4682)NTC_Voltage_Value = 4682;//101 d
+				if(TEMP_TABLE[i] > NTC_Voltage_Value){
 					NTC_TEM_Value = (int16_t)((i - 250 + 5)/10) + Current_TEMOffsetSetting;
 					ADC1_ITConfig(ADC1_IT_EOCIE, ENABLE);
 					break;
